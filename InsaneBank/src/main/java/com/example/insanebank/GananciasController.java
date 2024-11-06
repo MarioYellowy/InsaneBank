71e29cbcdf14ab0956196d5e28103fbb23d6231f
Juan Valentin Coria <valentin.coria@jala.university>
2024-11-06 19:21:57 -0300

    Añadi los botonoes de los dos tipos de graficos dy codifiqué que aparecezca uno u otro grafico dependiendo que boton se presione

--- InsaneBank/src/main/java/com/example/insanebank/GananciasController.java
+++ InsaneBank/src/main/java/com/example/insanebank/GananciasController.java
@@ -2,20 +2,40 @@ package com.example.insanebank;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-
-import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class GananciasController {
+
+    @FXML
+    private ImageView imageViewGrafico; // Imagen donde se mostrará el gráfico
+
+    private String graficoDeBarrasPath = "C:\\Users\\Valentin\\OneDrive\\Proyectos UNIVERSIDAD\\Calculo 1\\Capstone_Insane_Bank\\InsaneBank\\src\\main\\resources\\com\\example\\insanebank\\img\\graficos\\graficoBarras.png";  // Ruta del gráfico de barras
+    private String graficoLinealPath = "C:\\Users\\Valentin\\OneDrive\\Proyectos UNIVERSIDAD\\Calculo 1\\Capstone_Insane_Bank\\InsaneBank\\src\\main\\resources\\com\\example\\insanebank\\img\\graficos\\graficoLineal.png";  // Ruta del gráfico lineal
+
+    @FXML
+    void mostrarGraficoDeBarras(ActionEvent event) {
+        // Cambiar la imagen en el ImageView para mostrar el gráfico de barras
+        Image imagenGrafico = new Image("file:" + graficoDeBarrasPath);
+        imageViewGrafico.setImage(imagenGrafico);
+    }
+
+    @FXML
+    void mostrarGraficoDeLineal(ActionEvent event) {
+        // Cambiar la imagen en el ImageView para mostrar el gráfico lineal
+        Image imagenGrafico = new Image("file:" + graficoLinealPath);
+        imageViewGrafico.setImage(imagenGrafico);
+    }
+
 
     @FXML
     void showGnancias(ActionEvent event) {
 
     }
 
-
     @FXML
-    void showHome(ActionEvent event) throws IOException {
-        Validacion.cambiarVentana(event, "home-view");
+    void showHome(ActionEvent event) {
+
     }
 
 }
