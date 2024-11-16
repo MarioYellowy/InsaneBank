33235df631694d16a7e37fd3fe1715e45c8bff3a
RipMijael <Mijael.Rocha@jala.university>
2024-11-15 21:45:57 -0400

    pantalla de Ahorros

---
+++ InsaneBank/src/main/java/com/example/insanebank/AhorrosController.java
@@ -0,0 +1,21 @@
+package com.example.insanebank;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class AhorrosController {
+    @FXML
+    void mostrarGraficoDeBarras(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarGraficoDeLineal(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showGnancias(ActionEvent event) {
+
+    }
+}
